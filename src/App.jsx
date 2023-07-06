dae4eb1f7bf316b347e1582a5294e61e60b43f70
shivi <tanushukla1332@gmail.com>
2023-07-06 13:39:02 +0530

    create signIn page withUseState help

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 
 import './App.css'
-import { useState } from 'react'
 import Use from './Component/Use'
 
 function App() {
 
-  const count="shivani"
 
 
   return (
     <>
-    <Use  count={count}/>
-      
-
+    <Use/>
     </>
   )
 }
-
 export default App
--- src/Component/Use.jsx
+++ src/Component/Use.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Use(prop) {
+export default function Use() {
+    const[name,setName]=useState()
+    const[number,setNumber]=useState()
+    const[email,setEmail]=useState()
+
+    const data={
+        name:name,
+        number:number,
+        email:email,
+    }
+
+    function SubmitData(){
+        localStorage.setItem('userData', JSON.stringify(data) )
+        setName("")
+        setEmail("")
+        setNumber("")
+    }
+   
+
   return (
-    <div>Use{prop.count}</div>
+    <div style={{backgroundColor :"red" ,padding :"100px"}}>
+        <h3>SigIn form</h3>
+
+        <input type="text"  placeholder='enter user name' value={name} onChange={(e)=>setName(e.target.value)}/> <br />
+        <br /><br />
+        <input type="number"  placeholder='enter user contact number' value={number} onChange={(e)=>setNumber(e.target.value)}/> <br /><br /><br />
+        <input type="email" placeholder='enter user email'  value={email} onChange={(e)=>setEmail(e.target.value)}/> <br /><br /><br />
+
+        <button type="submit" onClick={SubmitData}> submit</button>
+
+    </div>
+    
   )
 }
